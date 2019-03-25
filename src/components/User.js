25e568713c6e38b4d14aa17e360e827c91068add
Matthew Wayock <wayock@gmail.com>
2019-03-25 06:03:45 -0400

    started user component

---
+++ src/components/User.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+
+}
+
+export default User;
